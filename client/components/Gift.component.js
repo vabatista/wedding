61a348813cfd6bf75c669ce7888c459d185821a6
Vitor A. Batista <vabatista@gmail.com>
2022-05-12 09:27:37 -0300

    gifts

--- client/components/Gift.component.js
+++ client/components/Gift.component.js
@@ -6,82 +6,51 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
+
+const gifts = [
+	{id: "medusa", src: "img/gifts/medusa.jpg", title: "Cortar a cabeça da medusa", 
+		desc: "Nos ajude a lutar junto com Perseu para combater a medusa.", price: "R$ 100"},
+	{id: "sistina", src: "img/gifts/sistina.jpg", title: "Pintar a Capela Sistina", 
+		desc: "Alguém aí quer pintar a Capela Sistina?", price: "R$ 200"},
+	{id: "ulisses", src: "img/gifts/ulisses.jpg", title: "Retornar a Ítaca", 
+		desc: "Embarque na Odisséia de Ulisses em seu retorno para Ítaca.", price: "R$ 300"},
+];
+
 export default function GiftCards() {
   return (
 
 	<Grid container spacing={2}>
-        <Grid item xs={4}>
-			<Card sx={{ maxWidth: 500 }}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						image="img/gifts/medusa.jpg"
-						alt="medusa"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h6" component="div">
-							Cortar a cabeça da medusa
-						</Typography>
-						<Typography color="text.secondary">
-							Nos ajude a lutar junto com Perseu para combater a medusa.
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary">
-						Quero contribuir (R$ 100,00)
-					</Button>
-				</CardActions>
-			</Card>
-        </Grid>
-        <Grid item xs={4}>
-			<Card sx={{ maxWidth: 500 }}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						image="img/gifts/sistina.jpg"
-						alt="Capela Sistina"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h6" component="div">
-							Pintar a Capela Sistina
-						</Typography>
-						<Typography color="text.secondary">
-							Alguém aí quer pintar a Capela Sistina?
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary">
-						Quero contribuir (R$ 200,00)
-					</Button>
-				</CardActions>
-			</Card>
-		</Grid>	
-        <Grid item xs={4}>
-			<Card sx={{ maxWidth: 500 }}>
-				<CardActionArea>
-					<CardMedia
-						component="img"
-						image="img/gifts/ulisses.jpg"
-						alt="Retorno de Ulisses"
-					/>
-					<CardContent>
-						<Typography gutterBottom variant="h6" component="div">
-							Retorno a Ítaca
-						</Typography>
-						<Typography color="text.secondary">
-							Embarque na Odisséia de Ulisses em seu retorno para Ítaca.
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-				<CardActions>
-					<Button size="small" color="primary">
-						Quero contribuir (R$ 300,00)
-					</Button>
-				</CardActions>
-			</Card>
-		</Grid>			
+        
+			{
+				gifts.map(gift => (
+					<Grid item xs={4}>			
+					<Card sx={{ maxWidth: 500 }}>
+						<CardActionArea>
+							<CardMedia
+								component="img"
+								image={gift.src}
+								alt={gift.id}
+							/>
+							<CardContent>
+								<Typography gutterBottom variant="h6" component="div">
+									{gift.title}
+								</Typography>
+								<Typography color="text.secondary">
+									{gift.desc}
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<CardActions>
+							<Button size="small" color="primary">
+								Quero contribuir ({gift.price})
+							</Button>
+						</CardActions>
+					</Card>
+				</Grid>
+		
+				))
+			}
+
 	</Grid>
 
   );
